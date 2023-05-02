4650ac25ef42d64d737f886722620d6baa575e5a
https://github.com/lamnaimohamed/ <lamnaimohamed@gmail.com>
2023-05-02 14:39:53 +0100

    firest commit

---
+++ src/test/java/com/exemple/springtestapi/SpringTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.springtestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
